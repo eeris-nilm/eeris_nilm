a06fc8b1442a39e1de5f14ce367b1a1da53e70cb
Christos Diou <cdiou@hua.gr>
2021-05-01 12:13:39 +0300

    Minor updates. Preparing for testing and updates

--- eeris_nilm/app.py
+++ eeris_nilm/app.py
@@ -50,7 +50,7 @@ def create_app(conf_file):
     else:
         loglevel = eval('logging.' + config['eeRIS']['loglevel'])
 
-    logging.setLevel(loglevel)
+    logging.basicConfig(level=loglevel)
 
     # DB connection
     logging.info("Connecting to database")
